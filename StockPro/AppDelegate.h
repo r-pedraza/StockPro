1216807e120dc976f15f067d4de3e174cec4eed5
Raúl Pedraza <raulpedrazaleon@gmail.com>
2015-02-16 12:14:12 +0100

    Initial Commit

---
+++ StockPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockPro
+//
+//  Created by Raul Pedraza on 16/2/15.
+//  Copyright (c) 2015 Raul Pedraza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
